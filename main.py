83a76300ee505bd363206a810fef3f8a152ba505
Yahia Kemari <64375557+Yahiake@users.noreply.github.com>
2022-07-18 15:54:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
 while True:
-    i = float(input("\nEnter a number : "))
-    k = int(input("\nEnter the degree of root : "))
+    i = float(input("Enter a number : "))
+    k = int(input("Enter the degree of root : "))
     i = i**(1/k)
-    k_str = str(k)
-    if k_str.endswith("11"):
-        print("\nYour", k, "\bth degree root number is : ", i,"\n")
-    elif k_str.endswith("12"):
-        print("\nYour", k, "\bth degree root number is : " ,i, "\n")
-    elif k_str.endswith("13"):
-        print("\nYour", k ,"\bth degree root number is : ", i, "\n")
-    elif k_str.endswith("1"):
-        print("\nYour", k, "\bst degree root number is : ", i,"\n")
-    elif k_str.endswith("2"):
-        print("\nYour", k, "\bnd degree root number is : " ,i, "\n")
-    elif k_str.endswith("3"):
-        print("\nYour", k ,"\brd degree root number is : ", i, "\n")
-    else :
-        print("\nYour", k ,"\bth degree root number is : " , i , "\n")
+    print(f"Your degree {k} root number is: {i}")
     check = str(input("Do you want to quit or start again? enter Y to restart or another key to end: "))
-    if check == "y":
-        continue
-    elif check == "Y":
+    if check in ["y", "Y"]:
         continue
     else:
-        print("\nGoodbye!")
+        print("Goodbye!")
         exit()
