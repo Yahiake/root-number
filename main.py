c9b631e84574ec4821fca6b62c98cfd6fb090ebd
Yahia Kemari <64375557+Yahiake@users.noreply.github.com>
2022-07-18 14:40:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 while True:
     i = float(input("\nEnter a number : "))
-    k = float(input("\nEnter the degree of root : "))
+    k = int(input("\nEnter the degree of root : "))
     i = i**(1/k)
-    print("\nYour ", k," degree root number is : ", i, "\n")
+    if k%10 == 1:
+        print("\nYour", k, "st degree root number is : ", i,"\n")
+    elif k%10 == 2:
+        print("\nYour", k, "nd degree root number is : " ,i, "\n")
+    elif k%10 == 3:
+        print("\nYour", k ,"rd degree root number is : ", i, "\n")
+    else :
+        print("\nYour", k ,"th degree root number is : " , i , "\n") #ordinal numbers are not perfect, works well until 20 after that it prints th which is wrong in some cases
     check = str(input("Do you want to quit or start again? enter Y to restart or another key to end: "))
     if check == "y":
         continue
