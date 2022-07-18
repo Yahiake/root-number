f15b8980d4c210c3f3a7c83320e06b606d76fcfd
Yahia Kemari <64375557+Yahiake@users.noreply.github.com>
2022-07-18 15:30:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ while True:
     i = float(input("\nEnter a number : "))
     k = int(input("\nEnter the degree of root : "))
     i = i**(1/k)
-    if k%10 == 1:
+    k_str = str(k)
+    if k_str.endswith("11"):
+        print("\nYour", k, "\bth degree root number is : ", i,"\n")
+    elif k_str.endswith("12"):
+        print("\nYour", k, "\bth degree root number is : " ,i, "\n")
+    elif k_str.endswith("13"):
+        print("\nYour", k ,"\bth degree root number is : ", i, "\n")
+    elif k_str.endswith("1"):
         print("\nYour", k, "\bst degree root number is : ", i,"\n")
-    elif k%10 == 2:
+    elif k_str.endswith("2"):
         print("\nYour", k, "\bnd degree root number is : " ,i, "\n")
-    elif k%10 == 3:
+    elif k_str.endswith("3"):
         print("\nYour", k ,"\brd degree root number is : ", i, "\n")
     else :
-        print("\nYour", k ,"\bth degree root number is : " , i , "\n") #ordinal numbers are not perfect, works well until 20 after that it prints th which is wrong in some cases in english
+        print("\nYour", k ,"\bth degree root number is : " , i , "\n")
     check = str(input("Do you want to quit or start again? enter Y to restart or another key to end: "))
     if check == "y":
         continue
